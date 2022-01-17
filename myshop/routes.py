ce3ac8af7cf596b49f87c3e73824baa735aa97b6
aislamml <aislamml151@gmail.com>
2022-01-17 23:44:07 +0300

    set up templates and href

--- myshop/routes.py
+++ myshop/routes.py
@@ -4,4 +4,20 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+@app.route('/product-details')
+def product_details():
+    return render_template('product-details.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
